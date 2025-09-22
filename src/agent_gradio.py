2f4f945231ce89f3eded3fd64908807f2268c700
SimonLuo <alsm168@gamil.com>
2025-09-22 12:20:22 +0800

    Add web search checkbox option to the interface

--- src/agent_gradio.py
+++ src/agent_gradio.py
@@ -4,7 +4,7 @@ import uuid
 from superviser_graph.graph import graph as main_agent
 
 # 带 thread_id 的聊天函数
-async def rag_react_stream_with_user_and_thread(message, history, user_id, session_state):
+async def rag_react_stream_with_user_and_thread(message, history, user_id, session_state, web_search):
     # 检查是否已有thread_id，如果没有则生成新的
     if "thread_id" not in session_state:
         session_state["thread_id"] = str(uuid.uuid4())
@@ -14,13 +14,15 @@ async def rag_react_stream_with_user_and_thread(message, history, user_id, sessi
     print(f"user_id = {user_id}")
     print(f'thread_id={thread_id}')
     print(f'message={message}')
+    print(f'web_search={web_search}')
     config = {"configurable": {"thread_id": thread_id, "user_id": user_id}}
 
     # 先 yield 一个"稍等"提示
     yield "请稍等，我正在处理您的问题..."
 
     response = ""
-    async for message_chunk, metadata in main_agent.astream({"messages": [{"role": "user", "content": message}]}, config):
+    async for message_chunk, metadata in main_agent.astream({"messages": [{"role": "user", "content": message}],
+    "choose_web_search": web_search}, config):
         # 跳过工具输出
         if metadata["langgraph_node"] == "tools":
             continue
@@ -57,7 +59,8 @@ with gr.Blocks() as demo_block:
             type="messages",
             additional_inputs=[
                 gr.Textbox(placeholder="请输入您的 user_id", label="User ID"),
-                session_state],
+                session_state,
+                gr.Checkbox(label="启用网络搜索", value=False)],
             chatbot=gr.Chatbot(height=300, type="messages"),  # ← 修复这里
             textbox=gr.Textbox(placeholder="请输入你的问题", container=False, ),
             title="通用对话机器人",
@@ -70,9 +73,9 @@ with gr.Blocks() as demo_block:
 
         # 点击按钮时，清空 chatbot 内容并重置会话状态
         def clear_chat_and_session():
-            return None, {}
+            return None, {}, False
         
-        clear_btn.click(clear_chat_and_session, outputs=[chat.chatbot, session_state])
+        clear_btn.click(clear_chat_and_session, outputs=[chat.chatbot, session_state, chat.additional_inputs[2]])
     # radio.change(prefill_chatbot, inputs=radio, outputs=chat)
 
 # 启动 Gradio，无参数 queue()
